12c5c9e53170c1b284e335e7fd960d26b4c2a47c
weifeilong <weifeilong2013@gmail.com>
2018-08-02 17:50:28 +0800

    fix exit

--- skywalking/start.sh
+++ skywalking/start.sh
@@ -2,6 +2,10 @@
 
 # feilong <weifeilong2013@gmail.com>
 
-cd /data/es && nohup ./bin/elasticsearch -Ecluster.name=CollectorDBCluster -Enode.name=CollectorDBNode &> /dev/null &
+set -ex
 
+cd /data/es && nohup ./bin/elasticsearch -Ecluster.name=CollectorDBCluster -Enode.name=CollectorDBNode &> /dev/null &
 cd /data/skywalking && ./bin/startup.sh
+
+sleep 30
+tail -f /data/skywalking/logs/skywalking-collector-server.log
