5d4158e562cda648187d41376ec0a8231c4d33cf
weifeilong <weifeilong2013@gmail.com>
2018-08-06 15:13:20 +0800

    change ip

--- skywalking/start.sh
+++ skywalking/start.sh
@@ -8,12 +8,18 @@ password=$(date +%s | sha256sum | base64 | head -c 20 ; echo)
 echo $password > /data/password.txt
 sed -i "s/password: admin/password: $password/g" /data/skywalking/webapp/webapp.yml
 
-cd /data/es && nohup ./bin/elasticsearch -Ecluster.name=CollectorDBCluster -Enode.name=CollectorDBNode &> /dev/null &
+ip=$(ifconfig eth0 | awk '/inet/ && !/inet6/ {print $2}')
+
+cd /data/es && nohup ./bin/elasticsearch -Ecluster.name=CollectorDBCluster -Enode.name=CollectorDBNode -Enetwork.host=$ip &> /dev/null &
+sed -i "s/127.0.0.1/$ip/g" /data/skywalking/webapp/webapp.yml
+sed -i "s/localhost/$ip/g" /data/skywalking/config/application.yml
 
 # ensure es is running
 printf "Elasticsearch is starting"
-while true; do curl -I http://127.0.0.1:9200 &> /dev/null && break || printf "." && sleep 1; done
+while true; do curl -I http://$ip:9200 &> /dev/null && break || printf "." && sleep 1; done
 echo
+
+echo "Elasticsearch Application started successfully!"
 
 cd /data/skywalking && ./bin/startup.sh
 
